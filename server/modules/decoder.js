69b1155d659357ca55d121eec4bec28fe84cc8eb
Jeremy Leif <jeremy.leif@gmail.com>
2017-06-16 11:14:57 -0500

    fixed problems

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -1,15 +1,32 @@
 
 var pool = require('../modules/database-config');
 var admin = require("firebase-admin");
-// var logger = require('./logger');
 
 
+if(process.env.private_key) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: process.env.type,
+      project_id: process.env.project_id,
+      private_key_id: process.env.private_key_id,
+      private_key: process.env.private_key,
+      client_email: process.env.client_email,
+      client_id: process.env.client_id,
+      auth_uri: process.env.auth_uri,
+      token_uri: process.env.token_uri,
+      auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+      client_x509_cert_url: process.env.client_x509_cert_url
+    }),
+    databaseURL: "https://psp-group.firebaseio.com/", // replace this line with your URL
+  });
 
+} else {
+  admin.initializeApp({
+    credential: admin.credential.cert("./server/firebase-service-account.json"),
+    databaseURL: "https://psp-group.firebaseio.com/", // replace this line with your URL
+  });
+}
 
-admin.initializeApp({
- credential: admin.credential.cert("./server/firebase-service-account.json"),
- databaseURL: "https://psp-group.firebaseio.com/", // replace this line with your URL
-});
 /* This is where the magic happens. We pull the id_token off of the request,
 verify it against our firebase service account private_key.
 Then we add the decodedToken */
@@ -47,12 +64,12 @@ var tokenDecoder = function (req, res, next) {
         });
       });
     })
-      .catch(function (error) {
-        // If the id_token isn't right, you end up in this callback function
-        // Here we are returning a forbidden error
-        console.error('User token could not be verified:', error);
-        res.sendStatus(403);
-      });
+    .catch(function (error) {
+      // If the id_token isn't right, you end up in this callback function
+      // Here we are returning a forbidden error
+      console.error('User token could not be verified:', error);
+      res.sendStatus(403);
+    });
   } else {
     // Seems to be hit when chrome makes request for map files
     // Will also be hit when user does not send back an idToken in the header
@@ -62,5 +79,5 @@ var tokenDecoder = function (req, res, next) {
 }
 
 module.exports = {
- token: tokenDecoder,
+  token: tokenDecoder,
 };
